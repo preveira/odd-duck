83ca554fb87924ff6e49546fbdd3f89cadeed136
preveira <patrick.k.reveira@gmail.com>
2024-02-06 22:53:09 -0800

    completed lab12, added HTML, JS

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 console.log('I am loaded!!');
 
+const ctx = document.getElementById('myChart');
+
 let oddDucks = []; 
 let image1 = document.getElementById('image1');
 let image2 = document.getElementById('image2');
@@ -16,25 +18,26 @@ function Duck(url, name) {
   this.timesShown = 0;
 }
 
-let odd1 = new Duck('img/bag.jpg', 'bag');
-let odd2 = new Duck('img/banana.jpg', 'banana');
-let odd3 = new Duck('img/boots.jpg', 'boots');
-let odd4 = new Duck('img/breakfast.jpg', 'Breakfast');
-let odd5 = new Duck('img/bubblegum.jpg', 'gum');
-let odd6 = new Duck('img/cthulhu.jpg', 'green vampire');
-let odd7 = new Duck('img/dog-duck.jpg', 'duck-dog');
-let odd8 = new Duck('img/dragon.jpg', 'dragon');
-let odd9 = new Duck('img/pen.jpg', 'pen');
-let odd10 = new Duck('img/pet-sweep.jpg', 'pet');
-let odd11 = new Duck('img/scissors.jpg', 'pizza scissors');
-let odd12 = new Duck('img/shark.jpg', 'shark');
-let odd13 = new Duck('img/sweep.png', 'sweep');
-let odd14 = new Duck('img/tauntaun.jpg', 'taun');
-let odd15 = new Duck('img/unicorn.jpg', 'uni');
-let odd16 = new Duck('img/water-can.jpg', 'watercan');
-let odd17 = new Duck('img/wine-glass.jpg', 'wine');
-
-oddDucks.push(odd1, odd2, odd3, odd4, odd5, odd6, odd7, odd8, odd9, odd10, odd11, odd12, odd13, odd14, odd15, odd16, odd17); // Renamed from 'ducks'
+let duck1 = new Duck('img/bag.jpg', 'bag');
+let duck2 = new Duck('img/banana.jpg', 'banana');
+let duck3 = new Duck('img/bathroom.jpg', 'bathroom');
+let duck4 = new Duck('img/boots.jpg', 'boots');
+let duck5 = new Duck('img/breakfast.jpg', 'breakfast');
+let duck6 = new Duck('img/bubblegum.jpg', 'bubblegum');
+let duck7 = new Duck('img/chair.jpg', 'chair');
+let duck8 = new Duck('img/cthulhu.jpg', 'cthulhu');
+let duck9 = new Duck('img/dog-duck.jpg', 'dog-duck');
+let duck10 = new Duck('img/dragon.jpg', 'dragon');
+let duck11= new Duck('img/pen.jpg', 'pen');
+let duck12 = new Duck('img/pet-sweep.jpg', 'pet-sweep');
+let duck13 = new Duck('img/scissors.jpg', 'scissors');
+let duck14 = new Duck('img/shark.jpg', 'shark');
+let duck15 = new Duck('img/sweep.png', 'sweep');
+let duck16 = new Duck('img/tauntaun.jpg', 'tauntaun');
+let duck17 = new Duck('img/unicorn.jpg', 'unicorn');
+let duck18 = new Duck('img/water-can.jpg', 'water-can');
+let duck19 = new Duck('img/wine-glass.jpg', 'wine-glass');
+oddDucks.push(duck1, duck2, duck3, duck4, duck5, duck6, duck7, duck8, duck9, duck10, duck11, duck12, duck13, duck14, duck15, duck16, duck17, duck18, duck19);
 
 function renderImage(image, duck) {
   image.setAttribute('src', duck.url);
@@ -42,9 +45,9 @@ function renderImage(image, duck) {
   duck.timesShown++;
 }
 
-renderImage(image1, odd1);
-renderImage(image2, odd2);
-renderImage(image3, odd3);
+renderImage(image1, duck1);
+renderImage(image2, duck2);
+renderImage(image3, duck3);
 
 console.log(oddDucks);
 
@@ -71,7 +74,7 @@ let handleClick = function (event) {
   renderNewDucks();
   currentRound = roundCount(totalRounds, currentRound);
 }
-oddImages.addEventListener('click', handleClick);
+duckImages.addEventListener('click', handleClick);
 
 function roundCount(total, current) {
   if (current < total) {
@@ -109,4 +112,70 @@ function renderNewDucks() {
   renderImage(image1, randomDuck1);
   renderImage(image2, randomDuck2);
   renderImage(image3, randomDuck3);
+
+  image1.setAttribute('src', randomDuck1.url);
+  image2.setAttribute('src', randomDuck2.url);
+  image3.setAttribute('src', randomDuck3.url);
+
+  image1.setAttribute('alt', randomDuck1.name);
+  image2.setAttribute('alt', randomDuck2.name);
+  image3.setAttribute('alt', randomDuck3.name);
+
+  randomDuck1.timesShown++;
+  randomDuck2.timesShown++;
+  randomDuck3.timesShown++;
+}
+
+
+
+function getNames() {
+  let names = [];
+  for (let i = 0; i < oddDucks.length; i++) {
+    names.push(oddDucks[i].name);
+  }
+  return names;
+}
+
+function getClicks() {
+  let clicks = [];
+  for (let i = 0; i < oddDucks.length; i++) {
+    clicks.push(oddDucks[i].clicks);
+  }
+  return clicks;
+}
+
+function getViews() {
+  let views = [];
+  for (let i = 0; i < oddDucks.length; i++) {
+    views.push(oddDucks[i].timesShown);
+  }
+  return views;
+}
+
+let button = document.getElementById('result-button');
+button.addEventListener('click', viewChart);
+
+function viewChart() {
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: getNames(),
+      datasets: [{
+        label: '# of Clicks',
+        data: getClicks(),
+        borderWidth: 1
+      }, {
+        label: '# of Views',
+        data: getViews(),
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 }
